383e0acb3c2cec7417f5bff9f18c08e0fd0e13b5
Micu Viorel <viorel_m@hotmail.com>
2025-09-07 16:50:09 +0300

    Diverse

--- Bluetooth  Finder/Pages/MainPage.xaml.cs
+++ Bluetooth  Finder/Pages/MainPage.xaml.cs
@@ -13,15 +13,26 @@ namespace Bluetooth__Finder.Pages
             BindingContext = vm;
         }
 
+        //private async void OnOpenDevice(object sender, EventArgs e)
+        //{
+        //    if (sender is Button btn && btn.BindingContext is DeviceModel device)
+        //        await Navigation.PushAsync(new DevicePage(new DeviceViewModel(
+        //            Services.GetRequiredService<BleService>(),
+        //            Services.GetRequiredService<AlarmService>(),
+        //            device)));
+        //}
+
         private async void OnOpenDevice(object sender, EventArgs e)
         {
             if (sender is Button btn && btn.BindingContext is DeviceModel device)
-                await Navigation.PushAsync(new DevicePage(new DeviceViewModel(
-                    Services.GetRequiredService<BleService>(),
-                    Services.GetRequiredService<AlarmService>(),
-                    device)));
+                await Navigation.PushAsync(new DevicePage
+                {
+                    BindingContext = new DeviceViewModel(
+                        Services.GetRequiredService<BleService>(),
+                        Services.GetRequiredService<AlarmService>(),
+                        device)
+                });
         }
-
         //private async void OnSettings(object sender, EventArgs e)
         //    => await Navigation.PushAsync(new SettingsPage(
         //        new SettingsViewModel((BindingContext as MainViewModel)!)));
